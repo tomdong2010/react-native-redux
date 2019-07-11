8af595ea35ac55e58c9932e80846226b033f295d
1659791130@qq.com <1659791130@qq.com>
2019-07-11 15:32:09 +0800

    thunkMiddleware

--- src/store/ConfigureStore.js
+++ src/store/ConfigureStore.js
@@ -3,6 +3,13 @@ import { createStore, applyMiddleware } from 'redux';
 import thunkMiddleware from 'redux-thunk';
 import rootReducer from '../reducers/index';
 
+/**
+ *
+ *
+ * 因为这个asyncSayActionCreator返回的不是一个action
+ * ，而是一个function。这个返回值无法被reducer识别。
+ * 这时候需要在action和reducer之间架起一座桥梁
+ */
 const createStoreWithMiddleware = applyMiddleware(thunkMiddleware)(createStore);
 
 export default function configureStore(initialState) {
